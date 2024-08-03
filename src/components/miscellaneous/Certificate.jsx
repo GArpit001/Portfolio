e52520d78631895fffd779c2dcd0a51a72570a43
Arpit <arpitvermaetw@gmail.com>
2024-08-03 23:21:05 +0530

    Check the certificate  download button

--- src/components/miscellaneous/Certificate.jsx
+++ src/components/miscellaneous/Certificate.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 // import one from "../../assets/img/Google ADS.jpg"
 // import Two from "../../assets/img/WordPress.jpg"
 
-// import Google from "../../assets/pdf/Google Ads.pdf"
-// import WordPress from "../../assets/pdf/Website using WordPress.pdf"
+import Google from "../../assets/pdf/Google Ads.pdf"
+import WordPress from "../../assets/pdf/Website using WordPress.pdf"
 
 
 const Certificate = () => {
@@ -23,7 +23,7 @@ const Certificate = () => {
                 </a>
 
                 <div className='relative'>
-                    <a href="https://res.cloudinary.com/mycodeworld/image/upload/v1722704590/rfiyjyyt70zx56vc1wtw.jpg" download="Google Ads Certificate" title='Download Google Ads Certificate' className='bg-blue-600 inline-block w-14 text-center p-4 rounded-full absolute bottom-4  right-4'>
+                    <a href={Google} download="Google Ads Certificate" title='Download Google Ads Certificate' className='bg-blue-600 inline-block w-14 text-center p-4 rounded-full absolute bottom-4  right-4'>
                         <i className="fa-solid fa-download"></i>
                     </a>
                 </div>
@@ -49,11 +49,11 @@ const Certificate = () => {
 
                     <img src=" https://res.cloudinary.com/mycodeworld/image/upload/v1722704617/zjnvoyjpm3zo3d9iisub.jpg" className='hover:scale-110 transition-all ease-in duration-100' alt="" />
 
-                   
+
                 </a>
 
                 <div className='relative'>
-                    <a href="https://res.cloudinary.com/mycodeworld/image/upload/v1722704617/zjnvoyjpm3zo3d9iisub.jpg" download="Word Press Certificate" title='Download WordPress Certificate' className='bg-blue-600 inline-block w-14 text-center p-4 rounded-full absolute bottom-4  right-4 '>
+                    <a href={WordPress} download="Word Press Certificate" title='Download WordPress Certificate' className='bg-blue-600 inline-block w-14 text-center p-4 rounded-full absolute bottom-4  right-4 '>
                         <i className="fa-solid fa-download"></i>
                     </a>
                 </div>
